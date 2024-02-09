826cfbae1b1771f8f2bdf88b219cfd0bfa00328e
julia <jujuba.gaby.barbosa@gmail.com>
2024-02-08 21:57:37 -0300

    Ultima aula - Finalizando o projeto

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,4 @@
+import { ChangeEvent, useState } from 'react';
 import Logo from './assets/Logo.svg'; //<img src= { Logo }/>  aqui eu estou pegando a minha Logo que é um,a variavel que foi importada
 import { NewNoteCard } from './components/new-note-card';
 import { NoteCard } from './components/note-card';
@@ -11,11 +12,67 @@ import { NoteCard } from './components/note-card';
  * hp-px = para colocar 1px
  * from-black/60 = o / é para controlar a opacidade da cor
  * overflow-hidden = esconder as coisas
+ * no react os estados são as unicas coisas que vão mudar com alguma interação com o usuario
+ * Toda vez que o valor do estado mudar o react vai renderizar todo a função
+ * toLocaleLowerCase() : Deixando a busca case sensitivi
+ * md:px-0 tirando o pd para desktop mas deixando no mobile
  */
 
+interface Note { //Formato das informações
+  id: string,
+  date: Date,
+  content: string
+}
+
 export function App() { // Componentes  = Funções que devolvem HTML
+
+  const [seach, setSearch] = useState('') //busca
+  const [notes, setNotes] = useState<Note[]>(() => { //Aqui vai ser onde a nota não vai sumir quando der f5
+    const notesOnStorage = localStorage.getItem('notes');//Procurar no LocalStorage se eu tenho o intem notes
+
+    if (notesOnStorage) {
+      return JSON.parse(notesOnStorage) //Se eu tiver vou retornar os intem do notesOnStorage
+    }
+
+    return []// Vai devolver o valor inicial
+  })//Falo para o react que isso é um array que tera o formato de Note
+
+  function onNoteCreated(content: string) { //fução para salvar a nota e mostrar
+    const newNote = { // criando uma nova nota, mas com os mesmos conteudos
+      id: crypto.randomUUID(), //Ira trazer um id unico para cada nota
+      date: new Date(),
+      content, // vai vir do parametro
+    }
+
+    const notesArray = [newNote, ...notes] //estou pegando todas as minhas notas, organizando por ordem e add a nova nota (newNote)
+
+    setNotes(notesArray)
+    localStorage.setItem('notes', JSON.stringify(notesArray))//o LOcal storage não le texto, então transformo em um JSON para facilitar na leitura// Aqui é para salvar as notas quando der f5 e elas não sumirem
+
+  }
+
+  function onNoteDeleted(id: string) {
+    const notesArray = notes.filter(note => {
+      return note.id !== id
+    })
+
+    setNotes(notesArray)
+
+    localStorage.setItem('notes', JSON.stringify(notesArray))
+  }
+
+  function handleSearch(event: ChangeEvent<HTMLInputElement>) { //Fazendo a busca no input
+    const query = event.target.value
+
+    setSearch(query)
+  }
+
+  const filteredNotes = seach !== '' //Filtrando a busca
+    ? notes.filter(note => note.content.toLocaleLowerCase().includes(seach.toLocaleLowerCase())) // Eu quero buscar o conteudo que inclui na busca
+    : notes // buscar as notas normal
+
   return (
-    <div className='mx-auto max-w-6xl my-12 space-y-6'>
+    <div className='mx-auto max-w-6xl my-12 space-y-6 px-5'>
       <img src={Logo} alt='NLW Expert' />
 
       <form className='w-full'>
@@ -23,17 +80,19 @@ export function App() { // Componentes  = Funções que devolvem HTML
           type="text"
           placeholder='Busque em suas notas...'
           className='w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder: text-slate-500'
+          onChange={handleSearch}
         />
       </form>
       <div className='h-px bg-slate-700'></div>
 
-      <div className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px]'>
 
-        <NewNoteCard />
-        <NoteCard note={{ // Um objeto para falar sobre os dados das notas
-            date: new Date(),
-            content: 'Hello, world'
-          }} />
+        <NewNoteCard onNoteCreated={onNoteCreated} />
+
+        {filteredNotes.map(note => { // o map vai percorrer todas as notas
+          return <NoteCard key={note.id} note={note} onNoteDeleted={onNoteDeleted} />; // vai retornar um NoteCard com a nota em si e com os dados que edtão dentro do objeto // o key é do react e é para deixar mais perfemotica, pelo id que vou saber oq foi mudado
+        })}
+
       </div>
     </div>
   )// Isso seria um JSX 
--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -1,7 +1,7 @@
 import * as Dialog from '@radix-ui/react-dialog' // importando o modelo Diolog do Radix o * é para importar todos os componentes
 import { X } from 'lucide-react' //icon de fechar a nota
 import { ChangeEvent, FormEvent, useState } from 'react'
-import {toast} from 'sonner' // Importando a função do sonner para mostrar um aviso
+import { toast } from 'sonner' // Importando a função do sonner para mostrar um aviso
 
 /**
      * ring-2 - como se fosse uma borda, mas é um box-shadow
@@ -20,10 +20,18 @@ import {toast} from 'sonner' // Importando a função do sonner para mostrar um 
      * 
      */
 
-export function NewNoteCard() {
+interface NewNoteCardProps { // Este NewNoteCardProps recebe o onNoteCreated que é uma função que tem um conteudo e retorna um vazio
+  onNoteCreated: (content: string) => void;
+}
+
+let speechRecognition: SpeechRecognition | null = null;
+
+export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
   const [shouldShowOnboarding, setShouldShowOnboarding] = useState(true) //O estado para quando clicar no 'use apenas texto' sumir
   const [content, setContent] = useState('')
   //Aqui eu vou estar chamando a variavel setShouldShowOnboarding para poder trocar o estado dela para False, pois no react não posso apenas trocar o valor da variavel
+  const [isRecording, setIsRecording] = useState(false) //estado do gravador
+
   function handleStartEditor() {
     setShouldShowOnboarding(false)
   }
@@ -39,10 +47,68 @@ export function NewNoteCard() {
 
   function handleSaveNote(event: FormEvent) { //Aqui eu fiz para quando clicar em salvar, não fechar a aba
     event.preventDefault()
-    console.log(content)
+
+    if (content == '') { //para não deixar salvar sem conteudo
+      return
+    }
+
+    onNoteCreated(content)//passando o conteudo e salvando quando clicar em salvar e mostrando na tela
+
+    //Para Resetar depois de criar uma nota
+    setContent('')
+    setShouldShowOnboarding(true)//Fazendo voltar a tela de add nota
 
     toast.success('Nota criada com sucesso')
   }
+
+  function handleStartRecording() { //Gravar a nota usando API
+    const isSpeechRecognitionAPIAvailable = 'SpeechRecognition' in window //Testando a API de gravação no navegador
+      || 'webkitSpeechRecognition' in window
+
+    if (!isSpeechRecognitionAPIAvailable) {
+      alert('Infelizmente seu navegador não suporta a API de gravação!')
+      return
+    }
+
+    setIsRecording(true)
+    setShouldShowOnboarding(false)
+
+    const SpeechRecognitionAPI = window.SpeechRecognition || window.webkitSpeechRecognition
+
+    speechRecognition = new SpeechRecognitionAPI()
+
+    speechRecognition.lang = 'pt-BR'
+    speechRecognition.continuous = true //Gravando até eu pedir para gravar
+    speechRecognition.maxAlternatives = 1 //Colocando palavras semelhantes caso não entenda
+    speechRecognition.interimResults = true // Vai trazendo oq eu falo antes de eu terminar de falar
+
+    speechRecognition.onresult = (event) => { // A função vai ser chamda toda vez que a API reconhecer uma fala
+      const transcrition = Array.from(event.results).reduce((text, result) => {
+        return text.concat(result[0].transcript)
+      }, '')// Reduce: Para cada informação do Array, ele executa uma funçãoe eu passo um segundo parametro, que é o valor iniciar da informação que eu quero montar
+
+      setContent(transcrition)//Ele vai aparecer oq eu falo no text area
+    }
+
+    speechRecognition.onerror = (event) => { // Saber se tem erro
+      console.error(event)
+    }
+
+    speechRecognition.start()
+
+
+
+  }
+
+  function handleStopRecording() {
+    setIsRecording(false)
+
+    if (speechRecognition !== null) {
+      speechRecognition.stop()
+  }
+
+}
+
   return (
     <Dialog.Root>
       <Dialog.Trigger className='rounded-md text-left flex-col outline-none bg-slate-800 p-5 gap-3 overflow-hidden relative hover:ring-2 
@@ -57,13 +123,12 @@ export function NewNoteCard() {
 
       <Dialog.Portal>
         <Dialog.Overlay className='inset-0 fixed bg-black/60' />
-        <Dialog.Content className='overflow-hidden fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 h-[60vh] max-w-[640px] w-full
-                 bg-slate-700 rounded-md flex flex-col outline-none'>
+        <Dialog.Content className='overflow-hidden fixed md:left-1/2 inset-0 md:inset-auto md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:h-[60vh] md:max-w-[640px] w-full bg-slate-700 md:rounded-md flex flex-col outline-none'>
           <Dialog.Close className='absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100'>
             <X className='size-5' />
           </Dialog.Close>
 
-          <form onSubmit={handleSaveNote} className='flex-1 flex flex-col '>
+          <form className='flex-1 flex flex-col '>
             <div className='flex flex-1 flex-col gap-3 p-5'>
               <span className='text-sm  font-medium text-slate-300'>
                 Adicionar nota
@@ -71,25 +136,39 @@ export function NewNoteCard() {
 
               {shouldShowOnboarding ? ( // Isso é um IF, que fala assim, quando clicar no botão 'utilize..' mudar para uma TextArea
                 <p className='text-sm leading-6 text-slate-400'>
-                  Comece <button className='font-medium text-lime-500 hover:underline'> granvando uma nota</button> em áudio ou se preferir <button
-                    onClick={handleStartEditor} className='font-medium text-lime-500 hover:underline'>utilize apenas texto</button>.
+                  Comece <button type='button' onClick={handleStartRecording} className='font-medium text-lime-500 hover:underline'> granvando uma nota</button> em áudio ou se preferir <button type='button' onClick={handleStartEditor} className='font-medium text-lime-500 hover:underline'>utilize apenas texto</button>.
 
                 </p>
               ) : (
                 <textarea
                   autoFocus
                   className='text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none'
-                  onChange={handleContentChanged}
+                  onChange={handleContentChanged} //Observando as mudanças e salvando no content
+                  value={content} //Vai estar sempre com o seu valor e refleting o valor de content. Então depois de salvar ela volta ficar sem texto
                 />
               )}
             </div>
 
-            <button
-              type='submit'
-              className='w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500'
-            >
-              Salvar nota
-            </button>
+            {isRecording ? ( // Enquanto esta gravando
+              <button
+                type='button'
+                onClick={handleStopRecording}
+                className='w-full flex items-center justify-center gap-2 bg-slate-900 py-4 text-center text-sm text-slate-300 outline-none font-medium hover:text-slate-100'
+              >
+                <div className='size-3 rounded-full bg-red-500 animate-pulse' />
+                Gravando! (clique para interromper)
+              </button>
+            ) : (
+              <button
+                type='button'
+                onClick={handleSaveNote}
+                className='w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500'
+              >
+                Salvar nota
+              </button>
+            )}
+
+
           </form>
         </Dialog.Content>
       </Dialog.Portal>
